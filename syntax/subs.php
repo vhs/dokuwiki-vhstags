4c6bff5c4869bda783f7844438f0f6afc3f4905a
Cory Alder <coryalder@gmail.com>
2026-02-16 10:23:48 -0800

    back to nomarl ptype, try linebreak management

--- syntax/subs.php
+++ syntax/subs.php
@@ -66,10 +66,8 @@ const LOCATIONMAP_HTML = <<<EOD
 EOD;
 
 const SEARCH_HTML = <<<EOD
-<a id="navBarSearchReveal" onclick="document.getElementById('navBarSearchInput').style.display=''; this.style.display = 'none'; return true;">
-<svg xmlns="http://www.w3.org/2000/svg" class="mikio-iicon" viewBox="0 0 32 32" aria-hidden="true" style="fill:currentColor">
-<path d="M27 24.57l-5.647-5.648a8.895 8.895 0 0 0 1.522-4.984C22.875 9.01 18.867 5 13.938 5 9.01 5 5 9.01 5 13.938c0 4.929 4.01 8.938 8.938 8.938a8.887 8.887 0 0 0 4.984-1.522L24.568 27 27 24.57zm-13.062-4.445a6.194 6.194 0 0 1-6.188-6.188 6.195 6.195 0 0 1 6.188-6.188 6.195 6.195 0 0 1 6.188 6.188 6.195 6.195 0 0 1-6.188 6.188z"></path></svg>
-</a>
+<a id="navBarSearchReveal" onclick="document.getElementById('navBarSearchInput').style.display=''; this.style.display = 'none'; return true;"><svg xmlns="http://www.w3.org/2000/svg" class="mikio-iicon" viewBox="0 0 32 32" aria-hidden="true" style="fill:currentColor">
+<path d="M27 24.57l-5.647-5.648a8.895 8.895 0 0 0 1.522-4.984C22.875 9.01 18.867 5 13.938 5 9.01 5 5 9.01 5 13.938c0 4.929 4.01 8.938 8.938 8.938a8.887 8.887 0 0 0 4.984-1.522L24.568 27 27 24.57zm-13.062-4.445a6.194 6.194 0 0 1-6.188-6.188 6.195 6.195 0 0 1 6.188-6.188 6.195 6.195 0 0 1 6.188 6.188 6.195 6.195 0 0 1-6.188 6.188z"></path></svg></a>
 <div id="navBarSearchInput" style="display: none;">
 <input name="q" type="text" class="edit search_typeahead" title="[F]" accesskey="f" placeholder="Search" autocomplete="on" id="navsearch__in" value="">
 <button value="1" type="submit" title="Search">
@@ -117,7 +115,7 @@ class syntax_plugin_vhstags_subs extends DokuWiki_Syntax_Plugin {
   }
 
   function getPType(): string {
-    return 'block'; // or "block" depending on if we want paragraphs closed
+    return 'normal'; // or "block" depending on if we want paragraphs closed
   }
 
   function getSort():int {
